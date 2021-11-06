01bcc9ebc445d2a436570f3ce810fb9210618c5d
ChenXiao Liu <810196813@qq.com>
2021-11-06 17:56:04 -0500

    add time picker

--- DateTimeTest/ViewController.swift
+++ DateTimeTest/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import Amplify
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var dateTime: UITextField!
+    
+    private let datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        datePicker.preferredDatePickerStyle = .wheels
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: nil,
+            action: #selector(doneButtonPressed)
+        )
+        toolBar.setItems([doneButton], animated: true)
+        dateTime.inputAccessoryView = toolBar
+        dateTime.inputView = datePicker
     }
 
-
+    @objc private func doneButtonPressed() {
+        let date = datePicker.date
+        let formatter = DateFormatter()
+        formatter.timeZone = .current
+        formatter.locale = .current
+        // https://nsdateformatter.com/
+        formatter.dateFormat = "MM-dd-yyyy HH:mm:ss a"
+        dateTime.text = formatter.string(from: date)
+        view.endEditing(true)
+        
+        DispatchQueue.global().async {
+            let dateTime = Temporal.DateTime(date)
+            let item = DateTimeTest(dateTime: dateTime)
+            
+            Amplify.DataStore.save(item) { result in
+                switch(result) {
+                case .success(let savedItem):
+                    print("Saved item: \(savedItem.id)")
+                case .failure(let error):
+                    print("Could not save item to DataStore: \(error)")
+                }
+            }
+        }
+    }
 }
 
